3f802ca4f5c94097b268f075ba1de4c84c54e0a6
MorganDawe <mdawe@discoverygarden.ca>
2015-06-01 10:50:05 -0300

    Cleaned up utility function

--- template.php
+++ template.php
@@ -148,40 +148,48 @@ function hag_theme_preprocess_page(&$variables) {
   }
 }
 
+/**
+ * Used with design kit to change hex to RGBA.
+ *
+ * @param string $color
+ *   HEX color code.
+ *
+ * @param string $opacity
+ *   Opacity, number between 0 and 1.
+ *
+ * @return string
+ *   The RGBA color value.
+ */
 function hag_theme_hex2rgba($color, $opacity = false) {
+  $default = 'rgb(0,0,0)';
+  //Return default if no color provided
+  if(empty($color))
+    return $default;
 
-	$default = 'rgb(0,0,0)';
+    //Sanitize $color if "#" is provided
+    if ($color[0] == '#' ) {
+      $color = substr( $color, 1 );
+    }
 
-	//Return default if no color provided
-	if(empty($color))
-          return $default;
+    //Check if color has 6 or 3 characters and get values
+    if (strlen($color) == 6) {
+      $hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
+    } elseif ( strlen( $color ) == 3 ) {
+      $hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+    } else {
+      return $default;
+    }
+    //Convert hexadec to rgb
+    $rgb =  array_map('hexdec', $hex);
 
-	//Sanitize $color if "#" is provided
-        if ($color[0] == '#' ) {
-        	$color = substr( $color, 1 );
-        }
-
-        //Check if color has 6 or 3 characters and get values
-        if (strlen($color) == 6) {
-                $hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
-        } elseif ( strlen( $color ) == 3 ) {
-                $hex = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
+    //Check if opacity is set(rgba or rgb)
+    if ($opacity) {
+      if (abs($opacity) > 1)
+        $opacity = 1.0;
+        $output = 'rgba('.implode(",",$rgb).','.$opacity.')';
         } else {
-                return $default;
+        $output = 'rgb('.implode(",",$rgb).')';
         }
-
-        //Convert hexadec to rgb
-        $rgb =  array_map('hexdec', $hex);
-
-        //Check if opacity is set(rgba or rgb)
-        if($opacity){
-        	if(abs($opacity) > 1)
-        		$opacity = 1.0;
-        	$output = 'rgba('.implode(",",$rgb).','.$opacity.')';
-        } else {
-        	$output = 'rgb('.implode(",",$rgb).')';
-        }
-
         //Return rgb(a) color string
         return $output;
 }
